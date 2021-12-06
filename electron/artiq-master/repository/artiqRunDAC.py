fa9c8212c1bf8bf5816c04ac7428102d03544028
imsacksteder <imsacksteder@willamette.edu>
2021-12-06 13:31:39 -0800

    updates to artiqGuiTabs and articRunDAC

---
+++ electron/artiq-master/repository/artiqRunDAC.py
@@ -0,0 +1,37 @@
+import sys
+import os
+#import datetime import datetime
+import select
+from artiq.experiment import *
+from artiq.coredevice.ad9910 import AD9910, SyncDataEeprom
+from artiq.coredevice.ad53xx import AD53xx
+from artiq.master.databases import DeviceDB
+from artiq.master.worker_db import DeviceManager
+import time
+import numpy as np
+
+class Run_UpdateVoltages(EnvExperiment):
+    
+    def build(self):
+        self.setattr_device('core')
+        self.setattr_device('zotino0')
+    
+    @kernel
+    def run(self):
+        self.core.reset()
+        self.zotino0.init()
+        
+        voltages = self.get_dataset(key="dac_voltages")
+        
+      
+        for pin in range(len(voltages)):
+            delay(500*us)
+            self.zotino0.write_dac(pin,voltages[pin])
+        
+        #test first pins
+        '''
+        delay(500*us)
+        self.zotino0.write_dac(0,voltages[0])
+        self.zotino0.write_dac(1,voltages[1])
+        self.zontin0.load()
+        '''
